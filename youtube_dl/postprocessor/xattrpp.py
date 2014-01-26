2f36470b2e76d10851669806bd7b7951ba36718a
Orochimarufan <orochimarufan.x3@gmail.com>
2014-01-26 17:22:10 +0100

    Fix ENOSPC (no space left on device) errors in the xattr PP.
    Long descriptions not fitting into a FS block shouldn't error out the whole download
    
    Signed-off-by: Orochimarufan <orochimarufan.x3@gmail.com>

--- youtube_dl/postprocessor/xattrpp.py
+++ youtube_dl/postprocessor/xattrpp.py
@@ -8,6 +8,35 @@ from ..utils import (
     hyphenate_date,
 )
 
+# Preemtively check FS support
+def getmount(path):
+        path = os.path.realpath(os.path.abspath(path))
+        while path != os.path.sep:
+            if os.path.ismount(path):
+                return path
+            else:
+                path = os.path.abspath(os.path.join(path, os.pardir))
+        return path
+
+if os.path.exists("/proc/mounts"):
+    def getfs(path):
+        mount = getmount(path)
+        with open("/proc/mounts") as mtab:
+            for line in mtab:
+                dev, mp, fs = line.split()[:3]
+                if mp == mount:
+                    return fs
+else:
+    def getfs(path):
+        return None
+
+def fs_supports_long_xattrs(fs):
+    """ True = arbitary, False = all values must fit into a block, None = unknown """
+    if fs.startswith("ext"):
+        return False
+    elif os.name == "nt":
+        return True # NTFS ADS' are arbitary
+
 
 class XAttrMetadataPP(PostProcessor):
 
@@ -20,19 +49,35 @@ class XAttrMetadataPP(PostProcessor):
     # TODO:
     #  * capture youtube keywords and put them in 'user.dublincore.subject' (comma-separated)
     #  * figure out which xattrs can be used for 'duration', 'thumbnail', 'resolution'
+    #  * Handle the filesystem mess more elegantly
     #
 
     def run(self, info):
         """ Set extended attributes on downloaded file (if xattr support is found). """
+
+        # Write the metadata to the file's xattrs
+        self._downloader.to_screen('[metadata] Writing metadata to file\'s xattrs')
+
+        filename = info['filepath']
 
         # This mess below finds the best xattr tool for the job and creates a
         # "write_xattr" function.
         try:
             # try the pyxattr module...
             import xattr
+            import errno
 
             def write_xattr(path, key, value):
-                return xattr.setxattr(path, key, value)
+                try:
+                    return xattr.setxattr(path, key, value)
+                except OSError as e:
+                    if e.errno == errno.ENOSPC:
+                        self._downloader.report_warning("Extended attributes exceed reserved file system space. "
+                                                        "Some file systems try to fit all attributes into a single block. "
+                                                        "Skipping %s (%i bytes)" % (key, len(value)))
+                        return False
+                    else:
+                        raise
 
         except ImportError:
             if os.name == 'nt':
@@ -72,24 +117,60 @@ class XAttrMetadataPP(PostProcessor):
                             "Couldn't find a tool to set the xattrs. "
                             "Install either the python 'xattr' module, "
                             "or the 'xattr' binary.")
+                    return False, info
 
-        # Write the metadata to the file's xattrs
-        self._downloader.to_screen('[metadata] Writing metadata to file\'s xattrs')
+                # Protect from too long values
+                file_system = getfs(filename)
+                long_values = fs_supports_long_xattrs(file_system)
 
-        filename = info['filepath']
+                if long_values is not True and hasattr(os, "statvfs"):
+                    _real_write_xattr = write_xattr
+                    blocksize = os.statvfs(filename).f_bsize
+                    written = []
+
+                    if long_values is False:
+                        def write_xattr(filename, xattr, value):
+                            written.append(len(value))
+                            total = sum(written)
+                            if total > blocksize - 256:
+                                self._downloader.report_warning(("XAttr size approaches blocksize (%iB/%iB). "
+                                                                 "Your file system (%s) doesn't support longer extended attributes. "
+                                                                 "Skipping %s (%iB)") %
+                                                                (total, blocksize, file_system, xattr, written[-1]))
+                                written.pop()
+                                return False
+                            else:
+                                _real_write_xattr(filename, xattr, value)
+                    else:
+                        def write_xattr(filename, xattr, value):
+                            written.append(len(value))
+                            total = sum(written)
+                            if total > blocksize - 256:
+                                try:
+                                    _real_write_xattr(filename, xattr, value)
+                                except (subprocess.CalledProcessError, OSError):
+                                    self._downloader.report_warning(("XAttr size approaches blocksize (%iB/%iB). "
+                                                                     "Your file system%s might not support longer extended attributes. "
+                                                                     "Skipping %s (%iB)") %
+                                                                    (total, blocksize, " (%s)" % file_system if file_system is not None else "", xattr, written[-1]))
+                                    written.pop()
+                                    return False
+                            else:
+                                _real_write_xattr(filename, xattr, value)
 
         try:
-            xattr_mapping = {
-                'user.xdg.referrer.url': 'webpage_url',
-                # 'user.xdg.comment':            'description',
-                'user.dublincore.title': 'title',
-                'user.dublincore.date': 'upload_date',
-                'user.dublincore.description': 'description',
-                'user.dublincore.contributor': 'uploader',
-                'user.dublincore.format': 'format',
-            }
+            # Order is important here! Do description last because it's most likely to kill the blocksize boundary.
+            xattr_mapping = [
+                ('user.xdg.referrer.url', 'webpage_url'),
+                ('user.dublincore.title', 'title'),
+                ('user.dublincore.contributor', 'uploader'),
+                ('user.dublincore.date', 'upload_date'),
+                ('user.dublincore.format', 'format'),
+                ('user.dublincore.description', 'description'),
+                #('user.xdg.comment', 'description'),
+            ]
 
-            for xattrname, infoname in xattr_mapping.items():
+            for xattrname, infoname in xattr_mapping:
 
                 value = info.get(infoname)
 
